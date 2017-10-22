67f9f6b8966fb79c89297a97f385f2010bbf59b3
myron <zhainandb@sina.com>
2017-10-22 16:33:12 +0800

    Myron:  add zipf random generator

--- caching/utils/random_utils.py
+++ caching/utils/random_utils.py
@@ -1,4 +1,5 @@
 import random
+from scipy.stats import zipf
 
 
 DEFAULT_SIZE = 1
@@ -17,6 +18,30 @@ def randint_array(low_bound, high_bound, size=None, seed=0):
     size = size if size is not None else DEFAULT_SIZE
     array = list()
     for index in xrange(size):
-        random.seed(index + seed)
+        random.seed(index+seed)
         array.append(random.randint(low_bound, high_bound))
+    return array
+
+
+def zipf_array(a, low_bound=None, high_bound=None, size=None, seed=0):
+    """
+    Get random variables that satisfies Zipf law.
+
+    Args:
+        a(float): parameter of zipf law, > 1
+        low_bound(int): lowest bound
+        high_bound(int): highest bound
+        size(int): size of array
+        seed(int): the seed of random
+    """
+    size = size if size is not None else DEFAULT_SIZE
+    counter, array = -1, list()
+    while len(array) < size:
+        counter += 1
+        candidate = int(zipf.rvs(a, size=1, random_state=counter+seed))
+        if low_bound and candidate < low_bound:
+            continue
+        if high_bound and candidate > high_bound:
+            continue
+        array.append(candidate)
     return array
---
+++ tests/test_utils.py
@@ -0,0 +1,23 @@
+# -*- coding: UTF-8 -*-
+# **********************************************************************************#
+#     File:  Test cases of utils
+# **********************************************************************************#
+import unittest
+from caching.utils.random_utils import zipf_array
+
+
+class TestRandomUtils(unittest.TestCase):
+
+    def setUp(self):
+        self.a = 2
+        self.low_bound = 0
+        self.high_bound = 10
+        self.size = 100
+        self.seed = 0
+
+    def test_zipf_array(self):
+        result = zipf_array(self.a,
+                            low_bound=self.low_bound,
+                            high_bound=self.high_bound,
+                            size=self.size, seed=self.seed)
+        print len(result), result
